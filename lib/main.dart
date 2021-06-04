d3af0bd4a375490cb817ee7e9990010b6ab4add9
shahryarkhan95 <58082896+shahryarkhan95@users.noreply.github.com>
2021-06-04 21:36:20 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,25 @@ class BasicSliverAppBar extends StatelessWidget {
         body: CustomScrollView(
           slivers: [
             SliverAppBar(
-                backgroundColor: Colors.red,
-                expandedHeight: 200,
-                title: Text('My App Bar'),
-                leading: Icon(I)),
+              backgroundColor: Colors.red,
+              expandedHeight: 200,
+              title: Text('My App Bar'),
+              leading: Icon(Icons.arrow_back),
+              actions: [
+                Icon(Icons.settings),
+                SizedBox(width: 12),
+              ],
+            ),
+            buildImages(),
           ],
         ),
       );
+  Widget buildImages() => SliverToBoxAdapter(
+    child: GridView.builder (
+      gridDelegate SliverGridDelegateWithFixedCrossAxisCount     crossAxisCount:2,
+    ),
+    itemCount: 20,
+    itemBuilder: (context,index) => ImageWidget
+  ),
+  );
 }
